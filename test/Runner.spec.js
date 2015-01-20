e7d1b6d3658f67e33188555f0823dfc29638a7c8
Darlan Alves <me@darlanalv.es>
2015-01-20 04:12:13 -0200

    feat(Runner): add runner to execute the library on files and strings

--- test/Runner.spec.js
+++ test/Runner.spec.js
@@ -1,14 +1,34 @@
-/*describe('Runner', function() {
-	describe('#parseFile', function() {
-		it('should read and parse a JS file', function() {
+var assert = require('assert'),
+	lib = require('../lib/'),
+	Runner = lib.Runner,
+	fs = require('fs'),
+	sampleFile = __dirname + '/samples/filter.js',
+	sampleContent = fs.readFileSync(sampleFile),
+	AngularStub = require(__dirname + '/stub/AngularStub');
 
+describe('Runner', function() {
+	describe('#runOnFile', function() {
+		it('should read and modify the content of a JS file', function() {
+			var result = Runner.runOnFile(sampleFile);
+			checkResult(result);
 		});
 	});
 
-	describe('#parseString', function() {
-		it('should parse a JS file as a string', function() {
-
+	describe('#runOnString', function() {
+		it('should parse and modify a script in a string', function() {
+			var result = Runner.runOnString(sampleContent);
+			checkResult(result);
 		});
 	});
 });
-*/
+
+
+function checkResult(code) {
+	var fn = new Function('$module', code),
+		stub = new AngularStub(fn);
+
+	stub.runStub();
+
+	assert(typeof stub.filters.double === 'function', 'Filter not found');
+	assert(stub.filters.double.$inject[0] === 'MathHelper', 'Filter dependency not found');
+}
